6fa12d41e9c146208196b3fa4d3076f5319c1917
dpdahal <dp48dahal@gmail.com>
2021-02-08 10:41:49 +0545

    message

---
+++ custom.js
@@ -0,0 +1,135 @@
+'use strict'
+
+// console.log('test')
+
+/*
+
+
+ */
+
+
+// document.write("hello");
+
+// var x = 10;
+// var y;
+
+// console.log(x)
+// console.log(y)
+
+// var x = 'ram';
+// var name = 20;
+// var data=''
+
+// var userName='ramsddfsdfsd';
+// var myUserName;
+// var _ = ''
+//
+// var a1=20;
+
+// var a = 20
+
+// console.log(a)
+// document.write(a);
+// document.getElementById('test').innerHTML = a;
+
+// alert(a);
+
+// var name = 'sophia';
+// var age = 20
+//
+// console.log(`my name is ${name} age ${age}`)
+
+// console.log('My name is '+name +' age'+age);
+
+// var a = 20
+// var b = 30
+// var c = 40
+//
+// if(a>b){
+//     if(a>c){
+//
+//     }
+// }
+//
+
+
+// console.log('test')
+
+
+// function test() {
+//     var age = 50
+//     if (age > 20) {
+//         return "success"
+//     } else {
+//         return "error"
+//     }
+//
+// }
+//
+// console.log(test())
+
+// var a = 10
+// var b = 20
+// var c = a
+
+//
+// function calculate(a, b = 0) {
+//     return a + b;
+//
+// }
+//
+// console.log(calculate(10))
+
+// function pr(data) {
+//     console.log(data)
+// }
+//
+//
+// function introduction(name, age = 0) {
+//     console.log(`Hello I am ${name}`)
+//
+//     if (age > 0) {
+//         pr(age)
+//     }
+// }
+//
+// introduction('Sophia', 20)
+
+// function $(id) {
+//     return document.getElementById(id);
+// }
+//
+// function getData() {
+//     let x = parseFloat($('num').value);
+//     let y = parseFloat($('num1').value);
+//     console.log(x + y);
+// }
+
+// setTimeout(function () {
+//     alert('test');
+// }, 3000);
+
+//
+// setInterval(function () {
+//     alert('test');
+// }, 3000);
+
+//
+// function loveGarKiNaGarne() {
+//     let question = confirm('Do you love me');
+//     if (question == true) {
+//         let name = prompt("Enter your name")
+//
+//     } else {
+//         alert("Kasto chuchii")
+//     }
+// }
+//
+//
+// setInterval(function () {
+//     loveGarKiNaGarne();
+// }, 3000);
+
+// let d = new Date()
+// console.log(d.toLocaleDateString())
+
